25458cc4c7d91e3ab3753c300d7d6cc11c4bdc60
aswandhkolangarodi <aswandhk2000@gmail.com>
2023-02-27 17:15:14 +0530

    web

--- web/urls.py
+++ web/urls.py
@@ -6,6 +6,5 @@ from django.views.generic import TemplateView
 app_name = "web"
 
 urlpatterns = [
-    # path("", views.index, name="index"),
-    # path("about/", TemplateView.as_view(template_name="web/about.html")),
+    path("", views.base, name="base"),
 ]
